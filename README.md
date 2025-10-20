0fc4cd7814fb6fbd826ea31093f1542287ab6487
Todd Cytra <tcytra@gmail.com>
2025-10-19 17:11:59 -0700

    Repository: Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Enraiged Tables
+
+**Enraiged Tables provides table handling features for Laravel Enraiged projects.**
+
+> **Please Note:** This package is currently a work in progress and not yet ready for production use.
+
+
+## License
+
+Enraiged Tables is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ src/TablesServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Enraiged\Tables;
+
+// use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class TablesServiceProvider extends ServiceProvider
+{
+    /**
+     *  Bootstrap the event services.
+     *
+     *  @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
